5e2cd52faf2ca3f03b9305708cff2e420e65de75
Carlos Lopez <celm1990@hotmail.com>
2019-08-22 16:06:52 -0500

    IMP: test

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name":  "TEST",
+    "summary":  "TEST",
+    "category":  "TEST",
+    "version":  "1.2.0",
+    "sequence":  1,
+    "author":  "TEST",
+    "description":  """""",
+    "depends":  ['sale_management'],
+    "data":  [
+    ],
+    "installable":  True,
+}
